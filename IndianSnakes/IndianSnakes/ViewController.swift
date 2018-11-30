8066331b8b1a0bec19b20ea7870c44b7a8f418b4
Raj Pawan Gumdal <gumdal@gmail.com>
2018-11-30 17:32:49 +0530

    Initial commit - coredata project on swift

---
+++ IndianSnakes/IndianSnakes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IndianSnakes
+//
+//  Created by Gumdal, Raj Pawan on 30/11/18.
+//  Copyright © 2018 Gumdal, Raj Pawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
